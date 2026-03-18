643dc6109dacc665fcacba8cb207e96ca9468854
Radjaye <mamadou-radjaye.sow.etu@univ-lille.fr>
2026-03-18 07:36:54 +0100

    diapo4 et projet

--- fichiers-exercices-javascript-diaporama/scripts/diapov2.js
+++ fichiers-exercices-javascript-diaporama/scripts/diapov2.js
@@ -5,18 +5,28 @@ let indiceImage = 0;
 
 function setuplisteners() {
     valInput.addEventListener('input', recupereValeurInput);
+
     lectureButton.addEventListener("click", lectureDiaporama);
     stopButton.addEventListener("click", stopDiaporama);
 }
+function afficheImage (indiceImage){
+    const images=document.getElementById("diapo");
+    images.src=tabImages[indiceImage];
+}
 
+function imageSuivante(){
+    indiceImage=(indiceImage+1)%tabImages.length;
+    afficheImage(indiceImage);
+}
 function recupereValeurInput() {
     const input = document.getElementById("delai");
-    const valeur = input.value;
+    const valeur =parseInt(input.value) ;
     return valeur;
 }
 function afficheinfo() {
     const image = document.getElementById("diapo");
     let indice=tabImages.indexOf(image.src);
+   
     const info = document.getElementById("info");
     info.textContent = (indice+1) + "/" + tabImages.length;
 }
@@ -27,6 +37,7 @@ function afficheNomImage() {
     const image = document.getElementById("diapo");
     let indice=tabImages.indexOf(image.src);
     let Image=tabImages[indice];
+    console.log(Image)
     const nomImage=Image.substring(7);
     const nomImageElement = document.getElementById("nomImage");
     nomImageElement.textContent = nomImage;
@@ -34,6 +45,7 @@ function afficheNomImage() {
 let montimer = null;
 function lectureDiaporama() {
     montimer=window.setInterval(imageSuivante,recupereValeurInput()*1000);
+    log.console(montimer)
 }
 function stopDiaporama() {
     window.clearInterval(montimer);
