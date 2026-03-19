5d221229222627fc66a620c3d4f2f98a96f3c698
Radjaye <mamadou-radjaye.sow.etu@univ-lille.fr>
2026-03-19 19:57:28 +0100

    tableau periodique

--- fichiers-exercices-javascript3/table periodique/table-periodique.js
+++ fichiers-exercices-javascript3/table periodique/table-periodique.js
@@ -1,1 +1,22 @@
-/* fonctions pour l'exercice sur la table périodique */
+const focus=document.getElementById("focus");
+const li=document.getElementsByClassName("elementChimique");
+const el=document.querySelector(".ligne .elementChimique");
+focus.innerHTML=el.innerHTML;
+focus.className=el.className;
+for (let i=1;i<li.length;i++){
+    li[i].addEventListener("mouseover",function(){
+        focus.innerHTML=this.innerHTML;
+        focus.className=this.className
+    })
+}
+
+const checkFocus=document.getElementById("checkFocus")
+checkFocus.addEventListener("change",cocher)
+
+function cocher(){
+    if (checkFocus.checked){
+        focus.style.display="block";
+    }else{
+        focus.style.display="none";
+    }
+}
